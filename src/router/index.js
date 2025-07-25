9481e39321aa09ebc6c1f4c8a21a801bb9dcce14
LeonardoDavidRY <ldramirez@uce.edu.ec>
2025-07-24 19:23:55 -0500

    router

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import CitaMedicaView from '@/views/CitaMedicaView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/cita-medica',
+    name: 'cita-medica',
+    component: CitaMedicaView
   },
   {
     path: '/about',
